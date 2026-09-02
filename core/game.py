c9f51a8b02af3964441eaad4c29e6ebc6bef6f9e
Vinicius Anjos <113810181+ViniAnjos84@users.noreply.github.com>
2026-09-02 01:05:00 -0300

    Restore gradual filling gameplay flow

--- core/game.py
+++ core/game.py
@@ -5,9 +5,10 @@ from core.config import (
     BACKGROUND_PATH,
     DRINK_ICON_SIZE,
     DRINK_POSITION,
+    FILL_DELAY_MS,
+    FILL_SPEED_ML_PER_SECOND,
     FPS,
     GLASS_POSITION,
-    INGREDIENT_ML_PER_CLICK,
     ML_BAR_POSITION,
     ML_BAR_SIZE,
     SCREEN_HEIGHT,
@@ -44,10 +45,13 @@ class Game:
 
         self.bebida = Bebida(copo=self.copo)
         self.item_selecionado = None
+        self.derramando = False
+        self.tempo_inicio = None
 
     def run(self):
         while self.running:
             self._process_events()
+            self._update()
             self._render()
             self.clock.tick(FPS)
 
@@ -60,24 +64,50 @@ class Game:
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self.running = False
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                self._select_ingredient(event.pos)
+                self._click_ingrediente(event.pos)
 
-    def _select_ingredient(self, position):
+    def _click_ingrediente(self, position):
+        """Primeiro clique inicia o preenchimento; segundo clique para e salva a quantidade."""
+        if self.derramando:
+            self._finalizar_preenchimento()
+            return
+
         ingrediente = self.slot_grid.item_at(position)
         if ingrediente is None:
             return
 
-        adicionado = self.bebida.adicionar_ingrediente(
-            ingrediente,
-            INGREDIENT_ML_PER_CLICK,
+        if self.bebida.adicionar_ingrediente(ingrediente):
+            self.item_selecionado = ingrediente
+            self.tempo_inicio = pygame.time.get_ticks()
+            self.derramando = True
+
+    def _quantidade_em_andamento(self):
+        if not self.derramando:
+            return 0
+
+        agora = pygame.time.get_ticks()
+        return self.bebida.quantidade_em_andamento(
+            self.tempo_inicio,
+            agora,
+            FILL_DELAY_MS,
+            FILL_SPEED_ML_PER_SECOND,
         )
 
-        if adicionado:
-            self.item_selecionado = ingrediente
-            print(
-                f"Ingrediente adicionado: {ingrediente.nome} "
-                f"(+{INGREDIENT_ML_PER_CLICK} ml)"
-            )
+    def _finalizar_preenchimento(self):
+        quantidade = self._quantidade_em_andamento()
+        self.bebida.confirmar_ingrediente(quantidade)
+        self.derramando = False
+        self.tempo_inicio = None
+
+    def _update(self):
+        if not self.derramando:
+            return
+
+        quantidade = self._quantidade_em_andamento()
+        if self.bebida.ml_atual + quantidade >= self.copo.capacidade_ml:
+            self.bebida.confirmar_ingrediente(quantidade)
+            self.derramando = False
+            self.tempo_inicio = None
 
     def _render(self):
         self.screen.blit(self.background, (0, 0))
@@ -88,6 +118,7 @@ class Game:
             self.screen,
             self.bebida,
             GLASS_POSITION,
+            quantidade_em_andamento=self._quantidade_em_andamento(),
         )
 
         if self.item_selecionado:
@@ -99,12 +130,22 @@ class Game:
                 rotation=90,
             )
 
+            if self.derramando:
+                self.bebida_view.draw_fluxo(
+                    self.screen,
+                    self.item_selecionado,
+                    DRINK_POSITION,
+                    DRINK_ICON_SIZE,
+                    self.tempo_inicio,
+                )
+
         self.copo_view.draw(self.screen, GLASS_POSITION)
         self.barra_ml.draw(
             self.screen,
             self.bebida.ml_atual,
             self.copo.capacidade_ml,
             ML_BAR_POSITION,
+            quantidade_em_andamento=self._quantidade_em_andamento(),
         )
 
         pygame.display.flip()
