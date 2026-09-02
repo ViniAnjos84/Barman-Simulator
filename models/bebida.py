8794b440ef8e644d205b0313068ba6efc55899b9
Vinicius Anjos <113810181+ViniAnjos84@users.noreply.github.com>
2026-09-02 01:04:49 -0300

    Restore gradual beverage quantity model

--- models/bebida.py
+++ models/bebida.py
@@ -24,20 +24,41 @@ class Bebida:
     def quantidades(self):
         return [item.quantidade_ml for item in self.ingredientes]
 
-    def adicionar_ingrediente(self, ingrediente, quantidade_ml):
-        if quantidade_ml <= 0 or self.copo is None:
+    def adicionar_ingrediente(self, ingrediente):
+        """Inicia um novo ingrediente com 0 ml para ser preenchido gradualmente."""
+        if self.copo is None or self.ml_atual >= self.copo.capacidade_ml:
             return False
 
-        espaco_disponivel = self.copo.capacidade_ml - self.ml_atual
-        quantidade_adicionada = min(quantidade_ml, espaco_disponivel)
-
-        if quantidade_adicionada <= 0:
-            return False
-
-        self.ingredientes.append(
-            IngredienteBebida(ingrediente, quantidade_adicionada)
-        )
+        self.ingredientes.append(IngredienteBebida(ingrediente, 0))
         return True
+
+    def quantidade_em_andamento(self, tempo_inicio, agora_ms, atraso_ms, velocidade):
+        """Retorna a quantidade do último ingrediente considerando o tempo de preenchimento."""
+        if not self.ingredientes or tempo_inicio is None:
+            return 0
+
+        tempo = agora_ms - tempo_inicio
+        if tempo <= atraso_ms:
+            return 0
+
+        quantidade = (tempo - atraso_ms) * velocidade / 1000
+        espaco = self.copo.capacidade_ml - self.ml_atual
+        return min(max(quantidade, 0), max(espaco, 0))
+
+    def confirmar_ingrediente(self, quantidade_ml):
+        """Grava a quantidade que foi preenchida no último ingrediente."""
+        if not self.ingredientes or self.copo is None:
+            return False
+
+        quantidade = max(0, quantidade_ml)
+        espaco = max(0, self.copo.capacidade_ml - self.ml_atual)
+        quantidade = min(quantidade, espaco)
+        self.ingredientes[-1].quantidade_ml += quantidade
+        return quantidade > 0
+
+    def remover_ultimo_ingrediente(self):
+        if self.ingredientes:
+            self.ingredientes.pop()
 
     def limpar(self):
         self.ingredientes.clear()
