3d20b992a117a4f823c26b8384d8b4c5256eb98e
Vinicius Anjos <113810181+ViniAnjos84@users.noreply.github.com>
2026-09-02 01:02:02 -0300

    Fix: add real ingredient quantity to drink

--- models/bebida.py
+++ models/bebida.py
@@ -24,8 +24,20 @@ class Bebida:
     def quantidades(self):
         return [item.quantidade_ml for item in self.ingredientes]
 
-    def adicionar_ingrediente(self, ingrediente, quantidade_ml=0):
-        self.ingredientes.append(IngredienteBebida(ingrediente, quantidade_ml))
+    def adicionar_ingrediente(self, ingrediente, quantidade_ml):
+        if quantidade_ml <= 0 or self.copo is None:
+            return False
+
+        espaco_disponivel = self.copo.capacidade_ml - self.ml_atual
+        quantidade_adicionada = min(quantidade_ml, espaco_disponivel)
+
+        if quantidade_adicionada <= 0:
+            return False
+
+        self.ingredientes.append(
+            IngredienteBebida(ingrediente, quantidade_adicionada)
+        )
+        return True
 
     def limpar(self):
         self.ingredientes.clear()
