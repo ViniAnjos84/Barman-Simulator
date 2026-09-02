8326dffe86562d647ab4200d66d8e5136117bcb5
Vinicius Anjos <113810181+ViniAnjos84@users.noreply.github.com>
2026-09-02 01:02:34 -0300

    Fix: restore cached glass transparency masks

--- core/assets.py
+++ core/assets.py
@@ -7,6 +7,7 @@ class AssetManager:
 
     def __init__(self):
         self._images = {}
+        self._masks = {}
 
     def image(self, path, size=None, rotation=0):
         key = (path, size, rotation)
@@ -26,6 +27,23 @@ class AssetManager:
 
     def image_from(self, directory, filename, size=None, rotation=0):
         return self.image(os.path.join(directory, filename), size, rotation)
+
+    def transparency_mask(self, path, size):
+        key = (path, size)
+        if key in self._masks:
+            return self._masks[key]
+
+        source = self.image(path, size)
+        mask = pygame.Surface(source.get_size(), pygame.SRCALPHA)
+
+        for x in range(source.get_width()):
+            for y in range(source.get_height()):
+                r, g, b, _ = source.get_at((x, y))
+                alpha = 255 if r == 255 and g == 255 and b == 255 else 0
+                mask.set_at((x, y), (255, 255, 255, alpha))
+
+        self._masks[key] = mask
+        return mask
 
 
 assets = AssetManager()
