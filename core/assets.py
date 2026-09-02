6e6aeff7a6cb9510963e93bc95eb64a8c340198c
Vinicius Anjos <113810181+ViniAnjos84@users.noreply.github.com>
2026-09-02 00:52:46 -0300

    Refactor: keep asset manager focused on cached images

--- core/assets.py
+++ core/assets.py
@@ -27,17 +27,5 @@ class AssetManager:
     def image_from(self, directory, filename, size=None, rotation=0):
         return self.image(os.path.join(directory, filename), size, rotation)
 
-    def transparency_mask(self, path, size):
-        mask = self.image(path, size)
-        transparency = pygame.Surface(mask.get_size(), pygame.SRCALPHA)
-
-        for x in range(mask.get_width()):
-            for y in range(mask.get_height()):
-                r, g, b, _ = mask.get_at((x, y))
-                alpha = 255 if r == 255 and g == 255 and b == 255 else 0
-                transparency.set_at((x, y), (255, 255, 255, alpha))
-
-        return transparency
-
 
 assets = AssetManager()
